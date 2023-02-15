28c39ba1d3182484339772275c8c2b9908ad98d3
KDissanayke-Bootcamp <ssndkavindra@gmail.com>
2023-02-15 20:37:46 +0530

    update 0001

--- src/Example.java
+++ src/Example.java
@@ -23,6 +23,7 @@ class Example{
     public static void main(String args[]){
         for(int i=0; i<10; i++){
             System.out.println(i+" ! : "+fact(i));
+            System.out.println();
         }
     }
 }
